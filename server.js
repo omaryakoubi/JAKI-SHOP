9c0d3568f84e3f2a58866b1cc40aa91d71ece338
Hamza Toukabri <59377594+HTOKO94@users.noreply.github.com>
2021-08-14 14:24:59 +0100

    Update server.js

--- server.js
+++ server.js
@@ -18,11 +18,16 @@ app.use('/api/orders/', orderRoute)
 
 if(process.env.NODE_ENV === 'production'){
 
-app.use('/' , express.static("/client/build"))
+app.use(express.static(path.join(__dirname,"/client/build")));
 
 app.get("*", (req, res)=>{
-  res.sendFile(path.resolve(__dirname, "/client/build/index.html"));
-});
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+})
+  else{
+    app.get("/" , (req, res) {
+            res.send("Api is Running")
+            });
+  }
 }
 
 const port = process.env.PORT || 5000;
